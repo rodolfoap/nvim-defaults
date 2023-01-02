f21926f53a1477553f2e6261de460d40ff1b183c
RodolfoAP <rodolfo.alcazar@gtd.eu>
2023-01-02 16:35:50 +0100

    Changes 2023/01/02-16:35:50
    Change-Id: Ib9bd21fce733eab7fd95fb3ab6065eb620e53e15

--- lua/r/plugins.lua
+++ lua/r/plugins.lua
@@ -1,11 +1,40 @@
--- Automatically run :PackerCompile whenever plugins.lua is updated with an autocommand:
-vim.api.nvim_create_autocmd('BufWritePost', {
-	group = vim.api.nvim_create_augroup('PACKER', { clear = true }),
-	pattern = 'plugins.lua',
-	command = 'source <afile> | PackerCompile',
-})
+-- See: https://www.chiarulli.me/Neovim-2/03-plugins/
 
-return require('packer').startup({function(use)
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  print("Installing packer close and reopen Neovim...")
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init({
+    display = {
+      open_fn = function()
+        return require('packer.util').float({ border = 'single' })
+      end
+    }
+  }
+)
+
+-- Install your plugins here
+return packer.startup(function(use)
 	-- Packer itself
 	use('wbthomason/packer.nvim')
 
@@ -16,7 +45,14 @@ return require('packer').startup({function(use)
 	use('vim-scripts/LanguageTool')
 
 	-- Autocomplete, nothing to do, enabled at startup
-	use('shougo/deoplete.nvim')
+	-- use('Shougo/deoplete.nvim', run = ':UpdateRemotePlugins')
+	-- Deoplete ++
+	-- use {'Shougo/deoplete.nvim', run = ':UpdateRemotePlugins'}
+	use 'deoplete-plugins/deoplete-tag'
+	use {'deathlyfrantic/deoplete-spell', branch = 'main'}
+	-- Deoplete relatives
+	use 'Shougo/neco-syntax'
+	use 'Shougo/context_filetype.vim'
 
 	-- Deoplete for Python
 	use('zchee/deoplete-jedi')
@@ -35,12 +71,9 @@ return require('packer').startup({function(use)
 
 	-- Autocomplete for spellcheck
 	use('deathlyfrantic/deoplete-spell')
-end,
-	config = {
-		display = {
-		open_fn = function()
-		return require('packer.util').float({ border = 'single' })
-		end,
-		},
-	},
-})
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require('packer').sync()
+  end
+end)
