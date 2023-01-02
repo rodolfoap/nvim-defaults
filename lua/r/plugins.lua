84545883c084f27136bd1bf2c69bf9589c6e90a1
RodolfoAP <rodolfo.alcazar@gtd.eu>
2023-01-02 16:09:28 +0100

    Changes 2023/01/02-16:09:28
    Change-Id: I3ebab1a5233a724a0626489f8b2c3157b447f99e

--- lua/r/plugins.lua
+++ lua/r/plugins.lua
@@ -1,39 +1,46 @@
 -- Automatically run :PackerCompile whenever plugins.lua is updated with an autocommand:
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-]])
+vim.api.nvim_create_autocmd('BufWritePost', {
+	group = vim.api.nvim_create_augroup('PACKER', { clear = true }),
+	pattern = 'plugins.lua',
+	command = 'source <afile> | PackerCompile',
+})
 
-return require('packer').startup(function(use)
-		-- Packer itself
-		use('wbthomason/packer.nvim')
+return require('packer').startup({function(use)
+	-- Packer itself
+	use('wbthomason/packer.nvim')
 
-		-- Left-column tree, activate with [CTRL-N]
-		use('scrooloose/nerdtree')
+	-- Left-column tree, activate with [CTRL-N]
+	use('scrooloose/nerdtree')
 
-		-- Grammar check, activate with [CTRL-L]
-		use('vim-scripts/LanguageTool')
+	-- Grammar check, activate with [CTRL-L]
+	use('vim-scripts/LanguageTool')
 
-		-- Autocomplete, nothing to do, enabled at startup
-		use('shougo/deoplete.nvim')
+	-- Autocomplete, nothing to do, enabled at startup
+	use('shougo/deoplete.nvim')
 
-		-- Deoplete for Python
-		use('zchee/deoplete-jedi')
+	-- Deoplete for Python
+	use('zchee/deoplete-jedi')
 
-		-- Deoplete for C++
-		use('sbdchd/neoformat')
+	-- Deoplete for C++
+	use('sbdchd/neoformat')
 
-		-- Deoplete for C++ from database, requires running rtags (rdm)
-		use('rzaluska/deoplete-rtags')
+	-- Deoplete for C++ from database, requires running rtags (rdm)
+	use('rzaluska/deoplete-rtags')
 
-		-- Nerdtree-like tree for latex tags, activate with [CTRL-T]
-		use('vim-voom/voom')
+	-- Nerdtree-like tree for latex tags, activate with [CTRL-T]
+	use('vim-voom/voom')
 
-		-- GNUPG files edition
-		use('jamessan/vim-gnupg')
+	-- GNUPG files edition
+	use('jamessan/vim-gnupg')
 
-		-- Autocomplete for spellcheck
-		use('deathlyfrantic/deoplete-spell')
-end)
+	-- Autocomplete for spellcheck
+	use('deathlyfrantic/deoplete-spell')
+end,
+	config = {
+		display = {
+		open_fn = function()
+		return require('packer.util').float({ border = 'single' })
+		end,
+		},
+	},
+})
